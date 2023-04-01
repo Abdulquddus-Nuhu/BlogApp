de67e3af5a2b82e36c9aa6c57460d8684ffc82bf
Abdulquddus-Nuhu <abdulquddusnuhu@gmail.com>
2023-04-01 22:33:39 +0100

    scafold identity & setup identity dbcontext

---
+++ src/Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Core.Entities;
+using Infrastructure.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data
+{
+    public class AppDbContext : IdentityDbContext<Persona, Role, Guid>
+    {
+        private readonly DbContextOptions<AppDbContext> _options;
+
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+            _options = options;
+        }
+
+        public DbSet<Blog> Blogs { get; set; }
+
+
+        public async Task<bool> TrySaveChangesAsync()
+        {
+            try
+            {
+                await SaveChangesAsync();
+
+                return true;
+            }
+            catch (DbUpdateException e)
+            {
+                Console.WriteLine(e.Message);
+                return false;
+            }
+        }
+
+    }
+}
+
